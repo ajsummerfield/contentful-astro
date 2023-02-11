aaeb6bd009f8fe62712c8bf68392cc80fbd3acc1
Andy Summerfield <aj.summerfield93@gmail.com>
2023-02-11 18:24:53 +0000

    Add new plugin with exported function to access the client

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 
 import type { AstroIntegration } from "astro";
+import type { ContentfulClientApi } from "contentful";
 import { vitePluginContentfulClient } from "./vitePluginContentfulClient";
 import { vitePluginContentfulComponents } from "./vitePluginContentfulComponents";
 
 export type IntegrationOptions = {
+  space: string;
+  accessToken: string;
   components?: object;
 };
+
+export const useContentfulClient = () : ContentfulClientApi => {
+  return globalThis.contentfulClient;
+}
 
 export default function contentfulIntegration(options: IntegrationOptions): AstroIntegration {
   return {
     name: "contentful-astro",
     hooks: {
-      "astro:config:setup": ({ updateConfig }) => {
+      "astro:config:setup": ({ updateConfig, injectScript }) => {
         updateConfig({
           vite: {
             plugins: [
-              vitePluginContentfulClient(),
+              vitePluginContentfulClient(options.space, options.accessToken),
               vitePluginContentfulComponents(options.components)
             ]
           },
         });
+
+        injectScript(
+          "page-ssr",
+          `
+            import { contentfulClient } from "virtual:contentful-client";
+            globalThis.contentfulClient = contentfulClient;
+          `
+        );
       },
     },
   };
